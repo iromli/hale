bf1b6b5475b8bc3b4ba9ccd2ab05bf38c6069547
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-02-01 07:00:59 +0700

    work in progress

---
+++ lib/Hale/Util.php
@@ -0,0 +1,38 @@
+<?php
+namespace Hale;
+
+/**
+ * Helper class.
+ *
+ */
+class Util
+{
+
+    /**
+     * Encodes a string to make it a URL-safe string.
+     *
+     * @param string Plain string
+     * @return string Encoded string
+     */
+    public static function base64Encode($str)
+    {
+        return trim(strtr(base64_encode($str), '+/', '-_'), '=');
+    }
+
+    /**
+     * Decodes a URL-safe string.
+     *
+     * @param string Decoded string
+     * @return string Plain string
+     */
+    public static function base64Decode($str)
+    {
+        return base64_decode(str_pad(
+            strtr($str, '-_', '+/'),
+            strlen($str) % 4,
+            '=',
+            STR_PAD_RIGHT
+        ));
+    }
+
+}
---
+++ tests/Hale/UtilTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Hale;
+
+use Hale\Util;
+
+class UtilTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testBase64Encode()
+    {
+        $haystack = Util::base64Encode('a secret string');
+        $this->assertNotContains('+', $haystack);
+        $this->assertNotContains('/', $haystack);
+        $this->assertNotContains('=', $haystack);
+    }
+
+    public function testBase64Decode()
+    {
+        $haystack = Util::base64Decode('-YSBzZWNyZXQgc3RyaW5n_');
+        $this->assertNotContains('-', $haystack);
+        $this->assertNotContains('_', $haystack);
+        $this->assertNotContains('=', $haystack);
+    }
+
+}
