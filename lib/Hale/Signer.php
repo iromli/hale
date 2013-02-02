39d1cd7ff9e2861ff999c87337bef0ad6e2fc462
Isman Firmansyah <isman.firmansyah@gmail.com>
2013-02-02 15:41:25 +0700

    more docblock

--- lib/Hale/Signer.php
+++ lib/Hale/Signer.php
@@ -1,6 +1,7 @@
 <?php
 namespace Hale;
 
+use \InvalidArgumentException;
 use \Hale\BadSignatureException;
 use \Hale\Util;
 
@@ -24,6 +25,12 @@ class Signer
         return $value . $this->sep . $this->getSignature($value);
     }
 
+    /**
+     * Unsigns the given string.
+     *
+     * @param string $signedValue
+     * @return string
+     */
     public function unsign($signedValue)
     {
         if (strpos($signedValue, $this->sep) === false) {
@@ -43,6 +50,13 @@ class Signer
         );
     }
 
+    /**
+     * This method is called to derive the key.
+     * If you're unhappy with the default key derivation choices you can override them here.
+     * Keep in mind that the key derivation in itsdangerous is not intended to be used
+     * as a security method to make a complex key out of a short password.
+     * Instead you should use large random secret keys.
+     */
     public function deriveKey()
     {
         switch ($this->keyDerivation) {
@@ -53,15 +67,37 @@ class Signer
             case 'hmac':
                 return hash_hmac('sha1', $this->salt, $this->secretKey, true);
             default:
-                throw new \InvalidArgumentException('Unknown key derivation method');
+                throw new InvalidArgumentException('Unknown key derivation method');
         }
     }
 
+    /**
+     * Returns the signature for the given value.
+     *
+     * @param string $value
+     * @return string Base64-encoded string
+     */
     public function getSignature($value)
     {
         $key = $this->deriveKey();
         $signature = hash_hmac('sha1', $value, $key, true);
         return Util::base64Encode($signature);
+    }
+
+    /**
+     * Just validates the given signed value.
+     *
+     * @param string $signedValue
+     * @return boolean Returns `true` if the signature exists and is valid, `false` otherwise.
+     */
+    public function validate($signedValue)
+    {
+        try {
+            $this->unsign($signedValue);
+            return true;
+        } catch (BadSignatureException $e) {
+            return false;
+        }
     }
 
 }
